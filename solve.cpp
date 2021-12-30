ccb7b0332a298e9e7d1fbb3fd3aa4b1297ca3675
Jayanta47 <jayantasadhu4557@gmail.com>
2021-12-30 20:17:05 +0600

    passenger distribution created

--- solve.cpp
+++ solve.cpp
@@ -6,8 +6,13 @@
 #include<unistd.h>
 #include<iostream>
 #include<fstream>
+#include<math.h>
+#include<random>
 
 using namespace std;
+
+#define passengers_per_hr 200
+#define total_sim_time 60 // in minutes
 
 // airport specifics holding variables
 
@@ -23,6 +28,8 @@ int t_y; // boarding at the gate
 int t_z; // walking on VIP channel on either direction
 
 
+int *passengerFreq;
+
 struct Passenger {
     int id;
     bool isVIP;
@@ -34,6 +41,36 @@ void * passengerThread(void* arg) {
 
     arg = nullptr;
     printf("passenger id : %d\n", p->id);
+}
+
+void poisson_dist_func() {
+    // const int nrolls = 10000; // number of experiments
+    const int nstars = passengers_per_hr;   // maximum number of stars to distribute
+
+    std::default_random_engine generator;
+    std::poisson_distribution<int> distribution(total_sim_time/2);
+
+    passengerFreq = new int[total_sim_time] {};
+
+    for (int i=0; i<nstars; ++i) {
+        int number = distribution(generator);
+        // cout<<number<<endl;
+        if (number<total_sim_time) ++passengerFreq[number];
+    }
+
+
+}
+
+
+void generatePassengerFreq() {
+    poisson_dist_func();
+    if (passengerFreq == nullptr){
+        cout<<"null"<<endl;
+    }
+    std::cout << "poisson_distribution:" << std::endl;
+    for (int i=0; i<total_sim_time; ++i)
+        std::cout << i << ": " << std::string(passengerFreq[i],'*') << std::endl;
+        // cout<<passengerFreq[i]<<endl;
 }
 
 void readInputFile(string fileName) {
@@ -68,13 +105,33 @@ void readInputFile(string fileName) {
 int main(void) {
 
     readInputFile("input.txt");
-
+    generatePassengerFreq();
+    // testFunc();
     pthread_t pt1;
     Passenger *p1 = new Passenger;
     p1->id = 10;
     p1->isVIP = false;
     pthread_create(&pt1, NULL, passengerThread, (void*)p1);
-    while(1);
 
     return 0;
 }
+
+
+// float nextTime()
+// {
+//     return -logf(1.0f - (float) random() / RAND_MAX ) / (passengers_per_hr/60.0);
+// }
+
+// void generatePassengerFreq() {
+//     int p[20] = {};
+
+//     for(int i=0;i<passengers_per_hr;i++) {
+//         int slot = round(nextTime());
+//         cout<<slot<<endl;
+//         p[slot]++;
+//     }
+
+//     for(int i=0;i<20;i++) {
+//         cout<<i<<" : "<<p[i]<<endl;
+//     }
+// }
